afb123fddfd56ecf8ec603ac0621e8d8b3f4893c
Leeroy Chakanyuka <168822625+leeroy-chakanyuka@users.noreply.github.com>
2026-08-26 21:46:54 +0200

    initial files as well as the application properties and up to date POM

---
+++ src/main/java/za/ac/cput/securitytutoring/model/User.java
@@ -0,0 +1,7 @@
+package za.ac.cput.securitytutoring.model;
+
+
+public class User {
+
+
+}
---
+++ src/test/java/za/ac/cput/securitytutoring/SecurityTutoringApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.cput.securitytutoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityTutoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
