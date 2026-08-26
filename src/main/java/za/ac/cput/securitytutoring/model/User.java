5d3f021d34110be176c8acc47086545fc46ef2f5
Leeroy Chakanyuka <168822625+leeroy-chakanyuka@users.noreply.github.com>
2026-08-26 22:37:19 +0200

    added the jpa entity and extended good security practice in the POM

--- src/main/java/za/ac/cput/securitytutoring/model/User.java
+++ src/main/java/za/ac/cput/securitytutoring/model/User.java
@@ -1,7 +1,25 @@
 package za.ac.cput.securitytutoring.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data // fills in the getters and setters for us
+@Builder // lets us build our object field for field
+@NoArgsConstructor // needed for jpa
+@AllArgsConstructor
+@Entity
 public class User {
-
+    @Id
+    @GeneratedValue // let it use the default - uses the TABLE enum for MYSQL
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String userame;
+    private String password;
 
 }
